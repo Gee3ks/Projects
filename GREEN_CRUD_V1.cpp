897a7b66199ef8071edbbbf376435537e5407e46
Gee3ks <145691689+Gee3ks@users.noreply.github.com>
2023-10-06 00:33:55 -0400

    Update GREEN_CRUD_V1.cpp

--- GREEN_CRUD_V1.cpp
+++ GREEN_CRUD_V1.cpp
@@ -8,46 +8,59 @@ private:
     const int TIME_PERIOD = 5;
 
 public:
-    int calculatePopulation(int initialSize, int days)
+    int calculatePopulation(long int initialSize, long int days)
     {
         if (days < 10)
             return days < 0 ? 0 : initialSize;
-        int a = initialSize, b = initialSize;
+        long int a = initialSize, b = initialSize;
         for (int i = 10; i <= days; i += TIME_PERIOD)
             a = (b += a) - a;
         return b;
     }
+
+    double getValue(string prompt)
+    {
+        double value(0);
+        do
+        {
+            cout << prompt;
+            if (cin >> value && value != 0)
+            {
+                break;
+            }
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cerr << "\nInvalid Input!";
+        } while (value <= 0);
+        return value;
+    }
 };
 
-int main(void)
+// Function repeat
+bool repeat(string prompt)
+{
+    string repeat = "";
+    do
+    {
+        cout << prompt;
+        cin >> repeat;
+
+    } while (repeat != "y" && repeat != "n");
+    return repeat == "y";
+}
+
+int main()
 {
     Crud crud;
-    while (true)
+    double INITIAL_SIZE;
+    int DAYS;
+    do
     {
-        int initialSize, days;
-        cout << "\nEnter the initial size of crud population in pounds: ";
-        if (cin >> initialSize && initialSize != 0)
-        {
-            cout << "\nEnter the number of days: ";
-            if (cin >> days)
-            {
-                int result = crud.calculatePopulation(initialSize, days);
-                cout << "\nAfter " << days << " days, the total population of Green Crud in pounds is " << result << " pounds\n";
-            }
-            else
-                cerr << "\nInvalid Input!";
-        }
-        else
-            cerr << "\nInvalid Input!";
+        INITIAL_SIZE = crud.getValue("\nEnter initial size of the green crud in pounds: ");
+        DAYS = crud.getValue("\nEnter the days: ");
 
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-
-        char repeat;
-        cout << "Do you want to calculate again? (y/n): ";
-        cin >> repeat;
-        if (repeat != 'y' && repeat != 'Y')
-            break;
-    }
+        cout << "\nAfter " << DAYS << " days the total Green Crud population is " << crud.calculatePopulation(INITIAL_SIZE, DAYS) << " lbs\n";
+    } while (repeat("\nDo you want to continue? (y/n): "));
+    cout << "\nExiting Program.";
     return 0;
 }
