9d0cab6bcbab78db6b5238f2825bfbefd8519915
Gee3ks <145691689+Gee3ks@users.noreply.github.com>
2023-10-06 00:35:06 -0400

    Update HYPOTENUSE.cpp

--- HYPOTENUSE.cpp
+++ HYPOTENUSE.cpp
@@ -1,83 +1,46 @@
-// Header
-// Directives
-#include <iostream>  // Directives for input and output
-#include <cmath>     // math library (sqrt(), pow()).
-using namespace std; // Reduce verbosity of the program
+// Headers / Directives
+#include <iostream>
+#include <cmath>
+using namespace std;
 
-// Class Triangle
-class Triangle
+// Generic Function for getting value
+double getValue(string message)
 {
-private:
-    // Private Class Properties
-    double BASE, HEIGHT, HYPOTENUSE;
-
-public:
-    // Class Constructor
-    // Initialize values to zero.
-    Triangle() : BASE(0), HEIGHT(0), HYPOTENUSE(0) {}
-
-    // class Methods
-
-    // Function to solve for Hypotenuse
-    double getH(double _base, double _height)
-    {
-
-        BASE = _base;
-        HEIGHT = _height;
-        HYPOTENUSE = sqrt(pow(BASE, 2) + pow(HEIGHT, 2));
-        return HYPOTENUSE;
-    }
-    // Function to make a triangle based on the user input values
-    void printTriangle()
-    {
-        // Decorators
-        for (int i = 1; i <= HEIGHT; ++i)
-        {
-            // Print Spaces
-            for (int j = 1; j <= HEIGHT - i; ++j)
-            {
-                cout << " ";
-            }
-
-            // Print Asterisks
-            for (int k = 1; k <= i; ++k)
-            {
-                cout << "*";
-            }
-
-            // Move to the next line
-            cout << '\n';
-        }
-        // Prints the class Properties in a formatted way.
-        cout << "\nBase: " << BASE;
-        cout << "\nHeight: " << HEIGHT;
-        cout << "\nHypotenuse: " << HYPOTENUSE << '\n';
-    }
-};
-
-// Main funtion
-int main(void)
-{
-    // Variable for the program to run
-    char repeat;
+    double VALUE;
     do
     {
-        cout << "\n------------T R I A N G L E ------------\n";
-        double base, height;
-        cout << "\nEnter the base: ";
-        cin >> base;
-        cout << "Enter the height: ";
-        cin >> height;
-        // Triangle Class Instance
-        Triangle t1;
-        t1.getH(base, height); // Access Triangle Class Method
-        t1.printTriangle();
+        cout << message;
+        cin >> VALUE;
+    } while (VALUE < 0);
+    return VALUE;
+}
 
-        // Prompt user if they want to continue the program (y/n).
-        cout << "\nDo you want to continue? (y/n): ";
+// Function for solving for hypotenuse
+double getHypotenuse(double BASE, double HEIGHT)
+{
+    double HYPOTENUSE = sqrt(pow(BASE, 2) + pow(HEIGHT, 2));
+    return HYPOTENUSE;
+}
+
+// calling main function
+int main(void)
+{
+    string message = "\n------------T R I A N G L E-------------\n";
+    string repeat;
+    do
+    {
+        cout << message;
+        double BASE = getValue("Enter the Base: ");
+        double HEIGHT = getValue("Enter the Height: ");
+        double HYPOTENUSE = getHypotenuse(BASE, HEIGHT);
+
+        cout << "\nBASE: " << BASE;
+        cout << "\nHEIGHT: " << HEIGHT;
+        cout << "\nHYPOTENUS: " << HYPOTENUSE;
+
+        // prompt user if they want continue the program
+        cout << "\n\nDo you want to continue? (y/n): ";
         cin >> repeat;
-
-    } while (repeat == 'y' || repeat == 'Y');
-    cout << "\nExiting Program.\n";
-    return 0;
+    } while (repeat == "y" || repeat == "Y");
+    cout << "\nExiting Program.";
 }
